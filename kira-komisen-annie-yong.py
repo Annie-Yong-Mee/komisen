4ec60b3ad05d6c435816dc3a5fbedd92f6712c0f
Annie-Yong-Mee <163367767+Annie-Yong-Mee@users.noreply.github.com>
2024-04-05 08:34:56 +0800

    Update and rename kira-komisen.py to kira-komisen-annie-yong.py 2

--- kira-komisen-annie-yong.py
+++ kira-komisen-annie-yong.py
@@ -1,10 +1,14 @@
+# Atur cara mengira komisen
+# Pengisytiharan pemboleh ubah
 jumlah = 0
 jualan = 0
 komisen = 0
 ulang = "Y"
 
 while ulang == "Y":
+    # Input
     jualan = float(input("Masukkan jumlah jualan: RM"))
+    # Proses
     if jualan > 80:
         kadar_komisen = 0.055
     elif jualan > 70:
@@ -15,11 +19,13 @@ while ulang == "Y":
         kadar_komisen = 0.03
     else:
         kadar_komisen = 0.02
-        
     komisen = jualan * kadar_komisen
+    # Output
     print ("Komisen anda ialah RM", round(komisen,2))
     jumlah = jumlah + komisen
+    # Input
     ulang = input("Masukkan Y untuk terus atau N untuk berhenti")
     
+# Output
 print ("\nJumlah koisen ialah RM", round (jumlah,2))
 print ("Sekian, tamat")
